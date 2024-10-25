e7dfc2acd2dca8f2dc9f8e5581fd03ebe6af68c5
Daniel Fichtinger <daniel@ficd.ca>
2024-10-25 17:04:07 -0400

    fix bandit.sh, add comments

--- bandit.sh
+++ bandit.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
+# Prerequisites: sshpass
+# bandit.labs.overthewire.org must be a trusted host, so ssh must be used once to manually connect to the server with creds bandit0:bandit0
+
 N="$1"
 counter=0
 
 while IFS= read -r line; do
   if [ "$counter" -eq "$N" ]; then
-    echo "$line"
     pass=$(echo "$line" | tr -d '\n')
   fi
   counter=$((counter + 1))
-done <./pass_bandit.txt
+done </home/fic/wargames/pass_bandit.txt
 
 # sshpass -p "$pass" echo \"ssh -p 2220 bandit"${N}"@bandit.labs.overthewire.org\"
 sshpass -p "$pass" ssh -p 2220 bandit"${N}"@bandit.labs.overthewire.org
