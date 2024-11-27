72ff89835f1f0f8f655d78023248c734850f05e6
Daniel Fichtinger <daniel@ficd.ca>
2024-11-26 20:07:54 -0500

    updated keyd config

---
+++ keyd/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo systemctl start keyd
+# We define our paths.
+LOCAL=~/scripts/keyd/default.conf
+BAK="$LOCAL.bak"
+MAIN=/etc/keyd/default.conf
+
+# We don't want to overwrite old backups; so we delimit each backup and
+# inject the current state of the MAIN file into the backup file.
+{
+  echo "BAKSTART"
+  cat $MAIN
+  echo "BAKEND"
+} >>$BAK
+
+echo "Backup created successfully."
+sudo cp -f $LOCAL $MAIN
+echo "Config overwritten."
+sudo keyd reload
+echo "Keyd daemon reloaded. Exiting..."
