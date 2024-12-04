dcb229acc3081f98f4d9d7552bd5ce070bdadf85
Daniel Fichtinger <daniel@ficd.ca>
2024-12-03 20:13:39 -0500

    fix obsidian bridge integration

--- obsidian-bridge.sh
+++ obsidian-bridge.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-OBSIDIAN_REST_API_KEY="$(pass show obsidian-bridge/api-key)"
+if [[ ! -f /tmp/.obsidian-api-key ]]; then
+  pass show obsidian-bridge/api-key >/tmp/.obsidian-api-key
+  chmod 600 /tmp/.obsidian-api-key
+fi
+OBSIDIAN_REST_API_KEY=$(cat /tmp/.obsidian-api-key)
 export OBSIDIAN_REST_API_KEY
