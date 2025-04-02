d4543ac2e1ba22ce2509e89e79e0fd3016061ebd
Daniel Fichtinger <daniel@ficd.ca>
2025-04-02 10:06:14 -0400

    added script for setting up ugrep

---
+++ setup-ugrep.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+sudo cp /usr/bin/ugrep /usr/local/bin/grep
+sudo cp /usr/bin/ugrep /usr/local/bin/egrep
+sudo cp /usr/bin/ugrep /usr/local/bin/fgrep
+sudo cp /usr/bin/ugrep /usr/local/bin/zgrep
+sudo cp /usr/bin/ugrep /usr/local/bin/zegrep
+sudo cp /usr/bin/ugrep /usr/local/bin/zfgrep
